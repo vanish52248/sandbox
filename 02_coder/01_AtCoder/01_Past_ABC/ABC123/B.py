208274ae5ab76d519e74f96ff07dd5ebfb337365
vanish52248 <vanish52248@gmail.com>
2022-12-30 20:21:19 +0900

    12/30AC

--- 02_coder/01_AtCoder/01_Past_ABC/ABC123/B.py
+++ 02_coder/01_AtCoder/01_Past_ABC/ABC123/B.py
@@ -1,31 +1,39 @@
-a = input()
-b = input()
-c = input()
-d = input()
-e = input()
-temp = [a, b, c, d, e]
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
 
-zero_lst = []
+lst = [a, b, c, d, e]
+ans = 0
+
+temp = 10000
 other_lst = []
+nine_flg = True
 
-for i in temp:
-    if i[-1] == "0":
-        zero_lst.append(int(i))
-    else:
-        other_lst.append(int(i))
-
-other_dct = {}
-for i in other_lst:
-    other_dct[i] = int(i)%10
-
-print(other_dct)
-
-other_sort_dct = dict(sorted(other_dct.items(), key=lambda x:x[1], reverse=True))
-
-ans = sum(zero_lst)
-for i, v in enumerate(other_sort_dct.keys()):
-    if i == len(other_lst)-1:
+min_ = 10000
+for i, v in enumerate(lst):
+    if str(v)[-1] == "0":
         ans += v
+        nine_flg = False
+    elif str(v)[-1] == "9":
+        ans += v + (10-int(str(v)[-1]))
     else:
-        ans += v + abs(int(str(v)[-1])%10-10)
-print(ans)
+        nine_flg = False
+        other_lst.append(v)
+        if temp > int(str(v)[-1]):
+            temp = int(str(v)[-1])
+            min_ = v
+
+if len(other_lst) > 0:
+    other_lst.remove(min_)
+
+for i in other_lst:
+    ans += i + (10-int(str(i)[-1]))
+
+if len(other_lst) > 0:
+    print(ans + min_)
+elif nine_flg:
+    print(ans - 1)
+else:
+    print(ans)
