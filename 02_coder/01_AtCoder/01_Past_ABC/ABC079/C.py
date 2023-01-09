d4ba62b031f776cb481bf5d8217e9ddc40b603a9
vanish52248 <vanish52248@gmail.com>
2023-01-09 18:48:23 +0900

    1/9AC

--- 02_coder/01_AtCoder/01_Past_ABC/ABC079/C.py
+++ 02_coder/01_AtCoder/01_Past_ABC/ABC079/C.py
@@ -1,4 +1,95 @@
-abcd = list(map(int,input()))
-ans = 7
-for i in abcd:
-    ans += i
+a = list(input())
+
+# + + +
+temp = 0
+lst = ["+", "+", "+"]
+for i in a:
+    temp += int(i)
+if temp == 7:
+    print(a[0]+lst[0]+a[1]+lst[1]+a[2]+lst[2]+a[3] + "=7")
+    exit()
+# + + -
+temp = 0
+lst = ["+", "+", "-"]
+for i, v in enumerate(a):
+    if i == 3:
+        temp -= int(v)
+    else:
+        temp += int(v)
+if temp == 7:
+    print(a[0]+lst[0]+a[1]+lst[1]+a[2]+lst[2]+a[3] + "=7")
+    exit()
+# + - +
+temp = 0
+lst = ["+", "-", "+"]
+for i, v in enumerate(a):
+    if i == 2:
+        temp -= int(v)
+    else:
+        temp += int(v)
+if temp == 7:
+    print(a[0]+lst[0]+a[1]+lst[1]+a[2]+lst[2]+a[3] + "=7")
+    exit()
+# - + +
+temp = 0
+lst = ["-", "+", "+"]
+for i, v in enumerate(a):
+    if i == 0:
+        temp = int(v)
+    elif i == 1:
+        temp -= int(v)
+    else:
+        temp += int(v)
+if temp == 7:
+    print(a[0]+lst[0]+a[1]+lst[1]+a[2]+lst[2]+a[3] + "=7")
+    exit()
+# - - -
+temp = 0
+lst = ["-", "-", "-"]
+for i, v in enumerate(a):
+    if i == 0:
+        temp = int(v)
+    else:
+        temp -= int(v)
+if temp == 7:
+    print(a[0]+lst[0]+a[1]+lst[1]+a[2]+lst[2]+a[3] + "=7")
+    exit()
+# - - +
+temp = 0
+lst = ["-", "-", "+"]
+for i, v in enumerate(a):
+    if i == 0:
+        temp = int(v)
+    elif i == 3:
+        temp += int(v)
+    else:
+        temp -= int(v)
+if temp == 7:
+    print(a[0]+lst[0]+a[1]+lst[1]+a[2]+lst[2]+a[3] + "=7")
+    exit()
+# - + -
+temp = 0
+lst = ["-", "+", "-"]
+for i, v in enumerate(a):
+    if i == 0:
+        temp = int(v)
+    elif i == 2:
+        temp += int(v)
+    else:
+        temp -= int(v)
+if temp == 7:
+    print(a[0]+lst[0]+a[1]+lst[1]+a[2]+lst[2]+a[3] + "=7")
+    exit()
+# + - -
+temp = 0
+lst = ["+", "-", "-"]
+for i, v in enumerate(a):
+    if i == 0:
+        temp = int(v)
+    elif i == 1:
+        temp += int(v)
+    else:
+        temp -= int(v)
+if temp == 7:
+    print(a[0]+lst[0]+a[1]+lst[1]+a[2]+lst[2]+a[3] + "=7")
+    exit()
