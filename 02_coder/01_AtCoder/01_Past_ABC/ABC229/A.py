b56cda540e4442c8293a02fe1ff2c9338092b745
vanish52248 <vanish52248@gmail.com>
2023-01-27 20:09:05 +0900

    1/27AC

--- 02_coder/01_AtCoder/01_Past_ABC/ABC229/A.py
+++ 02_coder/01_AtCoder/01_Past_ABC/ABC229/A.py
@@ -1,35 +1,8 @@
-# 2 行 2 列のグリッドを表す文字列
-grid_strings = ["##.", ".##"]
+s1 = list(input())
+s2 = list(input())
 
-# 黒マスを表す文字
-BLACK_CELL = "#"
-
-# 白マスを表す文字
-WHITE_CELL = "."
-
-# 頂点のリスト
-vertices = []
-
-# 辺のリスト
-edges = []
-
-# 文字列からグラフを構築する
-for i in range(len(grid_strings)):
-  for j in range(len(grid_strings[i])):
-    # 文字が黒マスを表す文字である場合
-    if grid_strings[i][j] == BLACK_CELL:
-      # 頂点を追加
-      vertices.append((i, j))
-
-      # 上下左右に隣接する頂点が存在するか調べる
-      for di, dj in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-        # 隣接する頂点が存在する場合
-        if (i + di, j + dj) in vertices:
-          # 辺を追加
-          edges.append(((i, j), (i + di, j + dj)))
-
-# 連通グラフかどうかを判定する
-is_connected = True
-
-# 各頂
-print(is_connected)
+if s1[0] == "#" and s1[1] == "." and s2[0] == "." and s2[1] == "#" \
+        or s1[0] == "." and s1[1] == "#" and s2[0] == "#" and s2[1] == ".":
+    print("No")
+else:
+    print("Yes")
