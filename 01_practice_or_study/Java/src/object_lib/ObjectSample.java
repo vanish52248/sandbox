4e7fe78d8bc44071ca68a50ee160a88d3958f3eb
vanish52248 <vanish52248@gmail.com>
2023-03-29 17:51:00 +0900

    object_lib追加

---
+++ 01_practice_or_study/Java/src/object_lib/ObjectSample.java
@@ -0,0 +1,21 @@
+public class ObjectSample {
+    public static void main(String[] args) {
+        Person p = new Person();
+        // 初期化された値を出力
+        // 暗黙的にSystem.out.println(p.toString())になる
+        System.out.println("① " + p);
+        p.setAge(20);
+        p.setName("hirano");
+        // 設定後の値を出力
+        // 暗黙的にSystem.out.println(p.toString())になる
+        System.out.println("② " + p);
+
+        // "clone()でインスタンスを複製
+        Person p2 = p.clone();
+        p.setAge(30);
+        p.setName("mikado");
+        // シャローコピー(参照のコピー)ではなくディープコピー(値のみのコピー)になっていることを確認
+        System.out.println("④ 変更した方 " + p);
+        System.out.println("④ 変更してない方 " + p2);
+    }
+}
