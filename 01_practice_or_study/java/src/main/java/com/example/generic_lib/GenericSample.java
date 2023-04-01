5e1892b28fd0eca78690ede7bba7998f06037664
vanish52248 <vanish52248@gmail.com>
2023-04-01 11:41:08 +0900

    ジェネリック型のサンプル追加

---
+++ 01_practice_or_study/java/src/main/java/com/example/generic_lib/GenericSample.java
@@ -0,0 +1,20 @@
+package com.example.generic_lib;
+
+// ①型が<E>のクラスを定義する(後から型を使いまわす場合)
+public class GenericSample<E> {
+    /*
+     * ジェネリクス(総称型)型のクラス定義
+     */
+    // ②メンバ変数として<E>型の変数を定義する
+    private E data;
+
+    // ③setterとして<E>型の変数にセットする
+    public void setE(E data) {
+        this.data = data;
+    }
+
+    // ④getterとして<E>型の変数を返す
+    public E getE() {
+        return this.data;
+    }
+}
