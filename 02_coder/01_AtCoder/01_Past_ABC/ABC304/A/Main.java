056cefed7e1bf0daaa49ef4496b9340bf8ff3fcf
vanish52248 <vanish52248@gmail.com>
2023-09-10 23:43:06 +0900

    commit¼れ

---
+++ 02_coder/01_AtCoder/01_Past_ABC/ABC304/A/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Integer N = sc.nextInt();
+        Map<String, Int>[] tempList = [];
+        for (int i = 0; i <= N; i++) {
+            Map<String, Int> map = new HashMap<>();
+            String S = sc.next();
+            String A = sc.next();
+            tempObj[S] = A;
+            tempList.push(tempObj);
+        }
+
+    }
+}
