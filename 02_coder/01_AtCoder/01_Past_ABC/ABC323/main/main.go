1d94834f2a1ceabd113cb0bf87e6d681a9f138ff
vanish52248 <vanish52248@gmail.com>
2024-03-20 15:46:20 +0900

    2024/03/20 AC

---
+++ 02_coder/01_AtCoder/01_Past_ABC/ABC323/main/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var S string
+	fmt.Scan(&S)
+
+	for i, v := range S {
+		if i+1 > 0 && (i+1)%2 == 0 {
+			fmt.Println(i, string(v))
+			if string(v) != "0" {
+				fmt.Println("No")
+				return
+			}
+		}
+	}
+	fmt.Println("Yes")
+}
