cc5c99c9fb822d089b5022ae2f52d5edf84fdf7b
vanish52248 <vanish52248@gmail.com>
2024-04-02 23:11:45 +0900

    1回目のコミット

--- 01_practice_or_study/go/src/main/main.go
+++ 01_practice_or_study/go/src/main/main.go
@@ -2,18 +2,8 @@ package main
 
 import "fmt"
 
-type Student struct {
-	name    string
-	math    float64
-	english float64
-}
-
-func (student Student) CallAvg() {
-	fmt.Println(student.name, (student.english+student.math)/2)
-}
-
 func main() {
-	s1 := Student{"test", 40, 50}
-	s1.CallAvg()
+
+	fmt.Println("commit1")
 
 }
