26901dfb0e6bfab5e27bea21cfeab05a01d7f5ff
vanish52248 <vanish52248@gmail.com>
2024-04-26 14:18:41 +0900

    2024/04/26 AC

--- 02_coder/01_AtCoder/01_Past_ABC/ABC345/main/main.go
+++ 02_coder/01_AtCoder/01_Past_ABC/ABC345/main/main.go
@@ -12,8 +12,6 @@ func main() {
 
 	var equals = S[1 : len(S)-1]
 
-	fmt.Println(equals)
-
 	if S[0] == '<' && S[len(S)-1] == '>' && strings.Count(equals, "=") == len(S)-2 {
 		fmt.Print("Yes")
 	} else {
